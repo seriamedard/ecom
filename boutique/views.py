d2ebc9153799d5b40d28ae37887b9cb8e18d8ea9
seriamedard <64494600+seriamedard@users.noreply.github.com>
2020-07-15 00:43:43 +0000

    views models tmepl

--- boutique/views.py
+++ boutique/views.py
@@ -1,26 +1,41 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-#import de tous les models
+# import de tous les models
 from .models import Categorie, Produit, SousCategorie
 
 """
 Le traitement des vues
 """
 
-#Listes des produits
+# Listes des produits
 class ListeProduit(ListView):
     model = Produit
-    context_object_name = 'produits'
+    context_object_name = 'liste_produits'
     template_name = 'boutique/liste.html'
     queryset = Produit.objects.filter(disponible=True).order_by('-date_de_creation')
+    devise = "FCFA"
+    def get_context_data(self, **kwargs):
+        context = super(ListeProduit, self).get_context_data(**kwargs)
+        context['devise'] = self.devise
 
 
+# Promotion
+class Promo(ListeProduit):
+    template_name = 'boutique/promo.html'
+    def get_queryset(self):
+        return Produit.objects.filter(disponible=True, promotion=True).order_by('-date_de_creation')
 
-#lites des produits d'un sous categorie
+class ListeCategorie(ListView):
+    model = Categorie
+    context_object_name = 'liste_categories'
+    template_name = 'boutique/categorie.html'
+
+
+# lites des produits d'un sous categorie
 class ListeProduitSousCategorie(ListView):
     model = Produit
-    context_object_name = 'sous_produits' #produit du sous categorie
+    context_object_name = 'liste_produits' #produit du sous categorie
     template_name = 'boutique/sous_produit.html'
 
     def get_queryset(self):
@@ -30,11 +45,11 @@ class ListeProduitSousCategorie(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ListeProduitSousCategorie, self).get_context_data(**kwargs)
-        context['sous_categories'] = SousCategorie.objects.all()
+        context['liste_sous_categories'] = SousCategorie.objects.all()
         return context
 
 
-#Detail du prduit
+# Detail du prduit
 class DetailProduit(DetailView):
     model = Produit
     context_object_name = 'un_produit'
@@ -52,11 +67,9 @@ def accueil(request):
     """
     liste des 15 produits les plus recents
     """
-    produits_recents = Produit.objects.filter(disponible=True).order_by('-date_de_creation')[:20]
-    categories_liste = Categorie.objects.all()
-    sous_categorie = SousCategorie.objects.all().order_by('nom') 
-    promo_liste = Produit.objects.filter(disponible=True, promotion=True)
-    #souscategorie = Categorie.objects.get(nom=cat).souscategorie_set.all()
+    liste_produits = Produit.objects.filter(disponible=True).order_by('-date_de_creation')[:20]
+    liste_categories = Categorie.objects.all()
+    liste_sous_categories = SousCategorie.objects.all().order_by('nom')
     return render(request, 'boutique/index.html', locals())
 
 #Vue recherche
