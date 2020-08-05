35216c5a6b878b7d53ada57f4cfd38e99fdb54a8
seriamedard <64494600+seriamedard@users.noreply.github.com>
2020-08-05 23:00:39 +0000

    stacticfiles add

--- ecom/settings.py
+++ ecom/settings.py
@@ -138,8 +138,11 @@ DEFAULT_CHARSET = 'utf-8'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # path media such as image
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
