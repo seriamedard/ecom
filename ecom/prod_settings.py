6d34c81cbdc72bedadd1c4cb2fc42a923fe866cf
seriamedard <64494600+seriamedard@users.noreply.github.com>
2020-08-05 23:24:00 +0000

    database

--- ecom/prod_settings.py
+++ ecom/prod_settings.py
@@ -1,8 +1,10 @@
 from .settings import *
+import dj_database_url
 
 DEBUG = False
 
 TEMPLATE_DEBUG = False
+DATABASES['default'] = dj_database_url.config()
 
 ALLOWED_HOSTS = ['somae.herokuapp.com']
 
