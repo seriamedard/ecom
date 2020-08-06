7d8991887e512ac0cbdf6476184699253be474d0
seriamedard <64494600+seriamedard@users.noreply.github.com>
2020-08-06 17:47:01 +0000

    storage dropbox

--- ecom/prod_settings.py
+++ ecom/prod_settings.py
@@ -12,3 +12,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 ALLOWED_HOSTS = ['somae.herokuapp.com','https://somae.herokuapp.com','media/images']
 
 SECRET_KEY = '$q_k_b=4q!py7d1^t3^b83xtff1_mr=2l_2po1)+x1)9rv$qy5'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+
+DROPBOX_OAUTH2_TOKEN = 'o4WwD5rIzSAAAAAAAAAAAfOKKdf82hfN03X9YA3WE0BVJYvlZa0zVgVsbs-1hpoR'
