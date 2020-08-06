e9d292791b4454932f9f1fc29456abf42deab303
seriamedard <64494600+seriamedard@users.noreply.github.com>
2020-08-06 18:44:10 +0000

    dropbox

--- ecom/prod_settings.py
+++ ecom/prod_settings.py
@@ -13,9 +13,3 @@ ALLOWED_HOSTS = ['somae.herokuapp.com','https://somae.herokuapp.com','media/imag
 
 SECRET_KEY = '$q_k_b=4q!py7d1^t3^b83xtff1_mr=2l_2po1)+x1)9rv$qy5'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-
-DROPBOX_OAUTH2_TOKEN = 'tpsk6m7heTAAAAAAAAAAAXh5_wXiZhX7m5QV44jjRL_AoiG93mIrH3f9f97rMzAb'
-
-DROPBOX_ROOT_PATH = 'media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace("\\", '/')
