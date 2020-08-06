8a591bda917a357d130c9774262ca789a8878e22
seriamedard <64494600+seriamedard@users.noreply.github.com>
2020-08-06 16:25:29 +0000

    update123

--- ecom/prod_settings.py
+++ ecom/prod_settings.py
@@ -4,11 +4,11 @@ import dj_database_url
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-ALLOWED_HOSTS = ['somae.herokuapp.com']
+ALLOWED_HOSTS = ['somae.herokuapp.com','https://somae.herokuapp.com','media/images']
 
 SECRET_KEY = '$q_k_b=4q!py7d1^t3^b83xtff1_mr=2l_2po1)+x1)9rv$qy5'
