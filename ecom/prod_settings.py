d01d1fdea8a3198a8d3bf6c66b97457bc013f7ca
seriamedard <64494600+seriamedard@users.noreply.github.com>
2020-08-16 14:49:27 +0000

    prod1

--- ecom/prod_settings.py
+++ ecom/prod_settings.py
@@ -9,7 +9,7 @@ DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-ALLOWED_HOSTS = ['somae.herokuapp.com','https://somae.herokuapp.com','somaele.osc-fr1.scalingo.io','https://somaele.osc-fr1.scalingo.io']
+ALLOWED_HOSTS = ['somaelect.herokuapp.com','https://somaelect.herokuapp.com','somaele.osc-fr1.scalingo.io','https://somaele.osc-fr1.scalingo.io']
 
 SECRET_KEY = '$q_k_b=4q!py7d1^t3^b83xtff1_mr=2l_2po1)+x1)9rv$qy5'
 
